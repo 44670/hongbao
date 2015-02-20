3077fbad3b72b655200ed078bfd2960d0b0b2a4f
44670 <44670@users.noreply.github.com>
2015-02-20 11:21:55 +0800

    remove unused modules

--- hongbao_3/app.js
+++ hongbao_3/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
-var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
